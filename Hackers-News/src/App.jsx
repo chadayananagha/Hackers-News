f556702ef6e7815bd6f38a9ca8e90a4a30c7ab50
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-03-27 16:32:10 +0100

    spinner added

--- Hackers-News/src/App.jsx
+++ Hackers-News/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "../src/components/Header";
 import SearchResult from "../src/components/SearchResult";
 import CircularProgress from '@mui/material/CircularProgress';
 // import Pagination from '@mui/material/Pagination';
-import { Container } from "@mui/material";
+// import { Container } from "@mui/material";
 function App() {
   const [datas, setDatas] = useState([]);
   const [isLoading, setIsLoading]=useState(true);
@@ -12,19 +12,25 @@ function App() {
 
   useEffect(() => {
     fetch("http://hn.algolia.com/api/v1/search?query=")
-      .then((response) => response.json())
+      .then((response) => {
+        if(!response.ok) throw new Error('${response.status}:Could not fetch the data');
+        return response.json();
+      })
       .then((datas) => {setDatas(datas.hits);
-      setIsLoading(false)});
+      setIsLoading(false)})
+      .catch((err) => console.error(err));
   }, []);
 
   return (
-    <Container maxWidth="sm">
+   
+<>
       <Header />
       {isLoading ? (<CircularProgress />):
       (<SearchResult datas={datas} />)
       }  
       {/* <Pagination count={10} />    */}
-    </Container>
+
+      </>
   );
 }
 export default App;
