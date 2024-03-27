f94d0f5b2cc96de345252bbb0096814280939511
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-03-27 19:01:52 +0100

    pagination added

--- Hackers-News/src/App.jsx
+++ Hackers-News/src/App.jsx
@@ -3,34 +3,67 @@ import { useEffect, useState } from "react";
 import Header from "../src/components/Header";
 import SearchResult from "../src/components/SearchResult";
 import CircularProgress from '@mui/material/CircularProgress';
-// import Pagination from '@mui/material/Pagination';
-// import { Container } from "@mui/material";
+import Pagination from '@mui/material/Pagination';
+import Typography from '@mui/material/Typography';
+import { Container } from "@mui/material";
+import axios from 'axios';
+
 function App() {
   const [datas, setDatas] = useState([]);
   const [isLoading, setIsLoading]=useState(true);
- 
-
+ const [totalPages,setTotalPages]=useState(0);
+ const [currentPage,setCurrentPage]=useState(1);
+ const[query,setQuery]=useState("");
+ const[searchInput,setSearchInput]=useState("");
+const handlePageChange = (event,value) =>
+{
+  console.log(event);
+  setCurrentPage(event.selected);
+  setCurrentPage(value);
+}
+const handleSubmit = event =>
+{
+  event.preventDefault();
+  setCurrentPage(1);
+  setQuery(searchInput);
+}
   useEffect(() => {
-    fetch("http://hn.algolia.com/api/v1/search?query=")
-      .then((response) => {
-        if(!response.ok) throw new Error('${response.status}:Could not fetch the data');
-        return response.json();
-      })
-      .then((datas) => {setDatas(datas.hits);
-      setIsLoading(false)})
-      .catch((err) => console.error(err));
-  }, []);
+    setIsLoading(true);
+    const fetchData =async() =>
+    {
+      try{
+       const { data } = await axios.get("http://hn.algolia.com/api/v1/search?query=",
+       {
+        params: {page:currentPage, query},
+       }
+      
+       );
+       const { hits, nbPages}=data;
+       setDatas(hits);
+       setTotalPages(nbPages);
+       console.log(data);
+      }catch(err)
+      {
+        console.log(err);
+      }finally{
+        setIsLoading(false);
+      }
+    
+    };
+    fetchData();
+  }, [currentPage, query]);
 
   return (
    
-<>
-      <Header />
+<Container>
+      <Header handleSubmit={handleSubmit} setSearchInput={setSearchInput} searchInput={searchInput} />
       {isLoading ? (<CircularProgress />):
       (<SearchResult datas={datas} />)
-      }  
-      {/* <Pagination count={10} />    */}
+      } 
+       <Typography>Page: {currentPage}</Typography> 
+       <Pagination count={totalPages} page={currentPage} onChange={handlePageChange} />   
 
-      </>
+      </Container>
   );
 }
 export default App;
--- Hackers-News/src/components/Header.jsx
+++ Hackers-News/src/components/Header.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -11,48 +10,10 @@ import Menu from "@mui/material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MoreIcon from "@mui/icons-material/MoreVert";
+import { Divider,Paper } from "@mui/material";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
-
-export default function Header() {
+export default function Header({handleSubmit,setSearchInput,searchInput}) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
@@ -149,15 +110,22 @@ export default function Header() {
           >
             HACKER'S NEWS
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
+          <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }} onSubmit={handleSubmit}
+    >
+       <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Hacker's News"
+        inputProps={{ 'aria-label': 'search google maps' }}
+        value={searchInput}
+              onChange={event => setSearchInput(event.target.value)}
+      />
+    </Paper>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
             <IconButton
@@ -191,41 +159,3 @@ export default function Header() {
     </Box>
   );
 }
-
-// // import React from 'react';
-// import * as React from 'react';
-// import Paper from '@mui/material/Paper';
-// import InputBase from '@mui/material/InputBase';
-// import Divider from '@mui/material/Divider';
-// import IconButton from '@mui/material/IconButton';
-// import SearchIcon from '@mui/icons-material/Search';
-// import Button from '@mui/material/Button';
-// import Stack from '@mui/material/Stack';
-
-// const Header = () => {
-//   return (
-//     <header>
-//       <Stack spacing={2} direction="row">
-//         <img src="public/images/899d76bbc312122ee66aaaff7f933d13.png" alt="hackers-news icon" width={50} height={50}></img>
-//         <h1>HACKERS NEWS</h1>
-
-//         <Paper
-//       component="form"
-//       sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-//     >
-//        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-//         <SearchIcon />
-//       </IconButton>
-//       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-//       <InputBase
-//         sx={{ ml: 1, flex: 1 }}
-//         placeholder="Search Hacker's News"
-//         inputProps={{ 'aria-label': 'search google maps' }}
-//       />
-//     </Paper>
-//     <Button variant="contained">Login</Button>
-//     </Stack>
-//     </header>
-//   )
-// }
-// export default Header;
