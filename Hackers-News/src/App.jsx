73e987995eb24593fba4c00833e2c1e79bca4d8a
OpheliaDanso <mayaafreh@gmail.com>
2024-03-27 12:24:16 +0100

    new header

--- Hackers-News/src/App.jsx
+++ Hackers-News/src/App.jsx
@@ -1,23 +1,23 @@
 // import React from 'react';
-import { useEffect, useState } from 'react';
-import Header from '../src/components/Header';
-import SearchResult from '../src/components/SearchResult';
-import Spinner from '../src/components/Spinner';
+import { useEffect, useState } from "react";
+import Header from "../src/components/Header";
+import SearchResult from "../src/components/SearchResult";
+import Spinner from "../src/components/Spinner";
 
 function App() {
-  const [datas,setDatas]=useState([]);
+  const [datas, setDatas] = useState([]);
   useEffect(() => {
     fetch("http://hn.algolia.com/api/v1/search?query=...")
-    .then(response => response.json())
-    .then(datas => setDatas(datas.hits))
-  },[])
+      .then((response) => response.json())
+      .then((datas) => setDatas(datas.hits));
+  }, []);
 
   return (
-    <div className=''>
+    <div className="">
       <Header />
       <Spinner />
       <SearchResult datas={datas} />
     </div>
-  )
+  );
 }
 export default App;
--- Hackers-News/src/components/Header.jsx
+++ Hackers-News/src/components/Header.jsx
@@ -1,38 +1,254 @@
-// import React from 'react';
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
 
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
 
-const Header = () => {
-  return (
-    <header>
-      <Stack spacing={2} direction="row">
-        <img src="public/images/899d76bbc312122ee66aaaff7f933d13.png" alt="hackers-news icon" width={50} height={50}></img>
-        <h1>HACKERS NEWS</h1>
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
 
-        <Paper
-      component="form"
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+export default function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
     >
-       <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search Hacker's News"
-        inputProps={{ 'aria-label': 'search google maps' }}
-      /> 
-    </Paper>
-    <Button variant="contained">Login</Button>
-    </Stack>
-    </header>
-  )
+      <MenuItem onClick={handleMenuClose}>LOGIN</MenuItem>
+      <MenuItem onClick={handleMenuClose}>SIGN-UP</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      {/* <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem> */}
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <img
+            src="public/images/899d76bbc312122ee66aaaff7f933d13.png"
+            alt="hackers-news icon"
+            width={50}
+            height={50}
+          ></img>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            HACKER'S NEWS
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
 }
-export default Header;
+
+// // import React from 'react';
+// import * as React from 'react';
+// import Paper from '@mui/material/Paper';
+// import InputBase from '@mui/material/InputBase';
+// import Divider from '@mui/material/Divider';
+// import IconButton from '@mui/material/IconButton';
+// import SearchIcon from '@mui/icons-material/Search';
+// import Button from '@mui/material/Button';
+// import Stack from '@mui/material/Stack';
+
+// const Header = () => {
+//   return (
+//     <header>
+//       <Stack spacing={2} direction="row">
+//         <img src="public/images/899d76bbc312122ee66aaaff7f933d13.png" alt="hackers-news icon" width={50} height={50}></img>
+//         <h1>HACKERS NEWS</h1>
+
+//         <Paper
+//       component="form"
+//       sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+//     >
+//        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+//         <SearchIcon />
+//       </IconButton>
+//       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+//       <InputBase
+//         sx={{ ml: 1, flex: 1 }}
+//         placeholder="Search Hacker's News"
+//         inputProps={{ 'aria-label': 'search google maps' }}
+//       />
+//     </Paper>
+//     <Button variant="contained">Login</Button>
+//     </Stack>
+//     </header>
+//   )
+// }
+// export default Header;
